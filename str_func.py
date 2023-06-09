bc9ac8f6cb8b747e3120de104e2c8e6afd42fd0d
Данила <spook9400@mail.ru>
2023-06-09 19:04:45 +0300

    get function title

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def func(string):
+    '''
+    function to uuper
+    :param string:
+    :return:
+    '''
     return string.upper()
+
+def func2(string):
+    '''
+    function title
+    :return:
+    '''
+    return string.title()
